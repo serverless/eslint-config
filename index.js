c4ebb3887149b8e7c55ceb86d6093b5602c3021d
Mariusz Nowak <mariusz@medikoo.com>
2019-06-07 15:05:28 +0200

    feat: Turn off rules that collide with Prettier

--- index.js
+++ index.js
@@ -3,5 +3,10 @@
 module.exports = {
   extends: "eslint:recommended",
   env: { es6: true },
-  parserOptions: { ecmaVersion: 2015 }
+  parserOptions: { ecmaVersion: 2015 },
+  rules: {
+    "no-extra-semi": "off", // Handled by Prettier
+    "no-mixed-spaces-and-tabs": "off", // Handled by Prettier
+    "no-unexpected-multiline": "off" // Handled by Prettier
+  }
 };
