be5561bd70f1849aa768313bcad65e82c30dc871
Mariusz Nowak <mariusz@medikoo.com>
2019-06-10 16:25:11 +0200

    feat: Reflect ESLint configuration of `serverless` package
    - Prepare dedicated 'browser' and 'node' entry points
    - Ensure no environment unrelated rules are applied
    - Skip options that conflict with Prettier

--- browser.js
+++ browser.js
@@ -5,8 +5,8 @@ const { join } = require('path');
 module.exports = {
   // The only way to ensure that ESLint resolves expected config from any location
   extends: join(__dirname, 'index.js'),
-  env: { node: true },
+  env: { browser: true },
   rules: {
-    'no-path-concat': 'error',
+    'no-alert': 'error',
   },
 };
