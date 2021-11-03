27b31849c1e5b9e936a92402641f487d30493246
Mariusz Nowak <medyk@medikoo.com>
2021-11-03 10:48:22 +0100

    fix: Recognize `BigInt` global
    While ES2020 feature, it is supported by Node.js v10

--- node/index.js
+++ node/index.js
@@ -5,6 +5,7 @@ const { join } = require('path');
 module.exports = {
   // The only way to ensure that ESLint resolves expected config from any location
   extends: join(__dirname, '../index.js'),
+  globals: { BigInt: true },
   env: { node: true },
   rules: {
     'no-path-concat': 'error',
