4d6663ce4fd7afeb2161ec155c15cb5971a13cdd
Mariusz Nowak <medyk@medikoo.com>
2022-05-23 14:10:37 +0200

    fix: Do not recognize `fetch` global in Node.js v12
    As it's supported starting from Node.js v18

--- node/index.js
+++ node/index.js
@@ -5,7 +5,7 @@ const { join } = require('path');
 module.exports = {
   // The only way to ensure that ESLint resolves expected config from any location
   extends: join(__dirname, '../index.js'),
-  globals: { BigInt: 'readonly', globalThis: 'readonly' },
+  globals: { BigInt: 'readonly', fetch: 'off', globalThis: 'readonly' },
   env: { node: true },
   rules: {
     'no-path-concat': 'error',
